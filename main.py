80de3ab8b9af1fc1b5a4700cb0126d2a4cf827aa
nvn0x33 <notnotnaveen@gmail.com>
2026-07-08 15:17:01 +0530

    feat: add extract audio feature

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from src import extract_audio
+
+input_path = "input/"
+output_path = "output/"
+temp_path = "temp/"
+
+video_files = os.listdir(input_path)
+extract_audio.extract(input_path, video_files[0], temp_path)
---
+++ src/extract_audio.py
@@ -0,0 +1,15 @@
+import ffmpeg
+
+def extract(inp_dir, vid_file, out_dir):
+    audio_path = out_dir + rm_extension(vid_file) + ".mp3"
+    
+    input_vid = ffmpeg.input(inp_dir+vid_file)
+    out_audio = ffmpeg.output(input_vid.audio, audio_path)
+    
+    ffmpeg.run(out_audio, overwrite_output=True)
+    
+def rm_extension(file_name):
+    file_name = file_name.split(".")
+    file_name.pop()
+    
+    return ".".join(file_name)
