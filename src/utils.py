05de8483f2dd8450e03dcf84cf7314f71d08d3c6
nvn0x33 <notnotnaveen@gmail.com>
2026-07-09 23:58:20 +0530

    feat: add get_resolution method to obtain height and width of video

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,24 @@
 import os
+import ffmpeg 
+
+def get_resolution(video_path):
+    probe = ffmpeg.probe(video_path)
+    video_stream = next(s for s in probe["streams"] if s["codec_type"] == "video")
+    return video_stream["width"], video_stream["height"]
 
 def rm_extension(file_name):
     file_name = file_name.split(".")
     file_name.pop()
     
     return ".".join(file_name)
+
+def RGB_to_BGR(hex="#FFGGHH"):
+    hex = hex.upper().strip("#")
+    
+    # format example: {\c&H0000FF&}
+    new_hex = f"&H{hex[4:6]}{hex[2:4]}{hex[0:2]}&"
+    
+    return new_hex
 
 def get_vid_files(target_dir):
     supported_exts = [".mp4", ".mkv", ".m4v", ".mov"]
