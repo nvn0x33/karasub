8103c8cd588d16558d618150e546edf1394fb091
nvn0x33 <notnotnaveen@gmail.com>
2026-07-09 21:47:17 +0530

    feat: add method to list video files in target dir

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
+import os
+
 def rm_extension(file_name):
     file_name = file_name.split(".")
     file_name.pop()
     
     return ".".join(file_name)
+
+def find_vid_files(target_dir):
+    supported_exts = [".mp4", ".mkv", ".m4v", ".mov"]
+    found_files = []
+    
+    for item in os.listdir(target_dir):
+        _, ext = os.path.splitext(item)
+        
+        if ext in supported_exts:
+            found_files.insert(ext)
+            
+    return found_files
