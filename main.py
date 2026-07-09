28331665e76b2bd7218aa4b2f70eeef301d785b0
nvn0x33 <notnotnaveen@gmail.com>
2026-07-09 23:59:47 +0530

    refactor: modify for pseudo code testing of UI.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
-from src import extract
-from src import Transcribe
+from src import extract, Transcribe, UI
 from src import get_vid_files
 
 # font size = min(width, height) * 0.085 // formula
 
-input_path = "input"
-output_path = "output"
-temp_path = "temp"
+input_dir = "input"
+output_dir = "output"
+temp_dir = "temp"
+font = "fonts/RobotoMono-Medium.ttf"
 
-video_files = get_vid_files(input_path)
+video_files = get_vid_files(input_dir)
 
 # pseudo code testings
-extracted_audio_path = extract(input_path, video_files[0], temp_path)
-print(extracted_audio_path)
-transcript = Transcribe(extracted_audio_path)
+
+new_ui = UI(font)
+
+# extracted_audio_path = extract(input_dir, video_files[0], temp_dir)
+# print(extracted_audio_path)
+# transcript = Transcribe(extracted_audio_path)
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .extract_audio import extract
 from .transcribe import Transcribe
 from .utils import *
+from .UI import UI
